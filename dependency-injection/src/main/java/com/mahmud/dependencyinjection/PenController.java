f3a6663b691a688f4a03a2aa3dc7dc12f149da52
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-02 01:45:34 +0600

    removed @AutoWired

--- dependency-injection/src/main/java/com/mahmud/dependencyinjection/PenController.java
+++ dependency-injection/src/main/java/com/mahmud/dependencyinjection/PenController.java
@@ -1,14 +1,12 @@
 package com.mahmud.dependencyinjection;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PenController {
     Pen pen;
-    
-    @Autowired
+
     public PenController(Pen pen) {
         this.pen = pen;
     }
