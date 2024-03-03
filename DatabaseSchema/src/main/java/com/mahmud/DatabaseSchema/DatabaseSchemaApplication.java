20cd4481e1f474223e58cfb3c3e5a8407bca8bf8
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-04 00:22:10 +0600

    connection completed

---
+++ DatabaseSchema/src/main/java/com/mahmud/DatabaseSchema/DatabaseSchemaApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.DatabaseSchema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseSchemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseSchemaApplication.class, args);
+	}
+
+}
---
+++ DatabaseSchema/src/test/java/com/mahmud/DatabaseSchema/DatabaseSchemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.DatabaseSchema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseSchemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
