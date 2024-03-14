941e68396db41b44c395bb0602130fff10c8d23c
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-14 22:34:44 +0600

    java based configs

--- spring-core-practice/src/main/java/com/mahmud/springcorepractice/SpringCorePracticeApplication.java
+++ spring-core-practice/src/main/java/com/mahmud/springcorepractice/SpringCorePracticeApplication.java
@@ -1,20 +1,29 @@
 package com.mahmud.springcorepractice;
 
+import com.mahmud.springcorepractice.configs.ServiceConfig;
 import com.mahmud.springcorepractice.greeter.Greeter;
 import com.mahmud.springcorepractice.greeter.impl.MorningGreeter;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+// import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+// import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class SpringCorePracticeApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("services.xml");
-		Greeter greeter =  context.getBean("morningGreeter", MorningGreeter.class);
-		Greeter greeter1 = context.getBean("morningGreeter", Greeter.class);
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(ServiceConfig.class);
+
+		Greeter greeter = context.getBean(MorningGreeter.class);
 
 		greeter.greet();
-		greeter1.greet();
+
+//		ApplicationContext context = new ClassPathXmlApplicationContext("services.xml");
+//		Greeter greeter =  context.getBean("morningGreeter", MorningGreeter.class);
+//		Greeter greeter1 = context.getBean("morningGreeter", Greeter.class);
+//		greeter1.greet();
+
+
+
 	}
 
 }
