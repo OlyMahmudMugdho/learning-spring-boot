8ada784f5330ade177e2f3baa104bbbd3f804da5
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-14 22:23:13 +0600

    initial setup

---
+++ spring-core-practice/src/main/java/com/mahmud/springcorepractice/SpringCorePracticeApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.springcorepractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCorePracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCorePracticeApplication.class, args);
+	}
+
+}
---
+++ spring-core-practice/src/test/java/com/mahmud/springcorepractice/SpringCorePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.springcorepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCorePracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
