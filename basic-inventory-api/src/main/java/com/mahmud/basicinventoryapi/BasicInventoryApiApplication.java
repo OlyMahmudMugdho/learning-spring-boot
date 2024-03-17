d6bf9c80e8d614d80d14faf03d5cce47c199c304
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-17 11:14:01 +0600

    initial setup

---
+++ basic-inventory-api/src/main/java/com/mahmud/basicinventoryapi/BasicInventoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.basicinventoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicInventoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicInventoryApiApplication.class, args);
+    }
+
+}
---
+++ basic-inventory-api/src/test/java/com/mahmud/basicinventoryapi/BasicInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.basicinventoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicInventoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
