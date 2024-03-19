e658afba2dd8fe874133b019b940edfc6a2f899a
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-19 14:32:37 +0600

    spring security starter project

---
+++ spring-security-learning/src/main/java/com/mahmud/springsecuritylearning/configs/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.mahmud.springsecuritylearning.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+    @Bean public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable())
+                .authorizeHttpRequests(authorizationManagerRequestMatcherRegistry -> authorizationManagerRequestMatcherRegistry.anyRequest().permitAll())
+                .build();
+    }
+}
---
+++ spring-security-learning/src/test/java/com/mahmud/springsecuritylearning/SpringSecurityLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.springsecuritylearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
