183bde8e2ddc53897b0fdad6ad8daa0b05124b4a
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-22 16:45:39 +0600

    simple modification

--- learning-mvc/src/main/java/com/mahmud/learningmvc/controllers/HomeController.java
+++ learning-mvc/src/main/java/com/mahmud/learningmvc/controllers/HomeController.java
@@ -21,12 +21,12 @@ public class HomeController {
 
         ArrayList<User> userList = new ArrayList<>();
 
-        User mugdho = new User(10,"mugdho","M. Oly Mahmud","mugdho@gmail.com");
-        User mila = new User(11,"mila","Maliha Ferdous Meghla","meghla@gmail.com");
-        User meghla = new User(12,"meghla","Meftahul Jannat Mila","meghla@gmail.com");
+        User mila = new User(1,"mila","Meftahul Jannat Mila","mila@gmail.com");
+        User mugdho = new User(2,"mugdho","M. Oly Mahmud","mugdho@gmail.com");
+        User meghla = new User(3,"meghla","Maliha Ferdous Meghla","meghla@gmail.com");
 
-        userList.add(mugdho);
         userList.add(mila);
+        userList.add(mugdho);
         userList.add(meghla);
 
         model.addAttribute("userList",userList);
