5d4edb43fad56476e06990907789937b0f334854
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-03-22 16:41:43 +0600

    added user controller

--- learning-mvc/src/main/java/com/mahmud/learningmvc/controllers/HomeController.java
+++ learning-mvc/src/main/java/com/mahmud/learningmvc/controllers/HomeController.java
@@ -16,5 +16,20 @@ public class HomeController {
         return "home";
     }
 
+    @GetMapping("/user")
+    public String user(Model model) {
 
+        ArrayList<User> userList = new ArrayList<>();
+
+        User mugdho = new User(10,"mugdho","M. Oly Mahmud","mugdho@gmail.com");
+        User mila = new User(11,"mila","Maliha Ferdous Meghla","meghla@gmail.com");
+        User meghla = new User(12,"meghla","Meftahul Jannat Mila","meghla@gmail.com");
+
+        userList.add(mugdho);
+        userList.add(mila);
+        userList.add(meghla);
+
+        model.addAttribute("userList",userList);
+        return "user";
+    }
 }
