93ee442b4781f3aaa552320a50aa67606051a69c
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-13 12:36:36 +0600

    init

---
+++ oauth2-custom/src/main/java/com/mahmud/oauth2custom/Oauth2CustomApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.oauth2custom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2CustomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2CustomApplication.class, args);
+	}
+
+}
---
+++ oauth2-custom/src/test/java/com/mahmud/oauth2custom/Oauth2CustomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.oauth2custom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2CustomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
