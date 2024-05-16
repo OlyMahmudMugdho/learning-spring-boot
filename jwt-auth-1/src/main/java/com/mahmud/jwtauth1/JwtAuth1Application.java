6d9c4f3a60489f5da8a383e82a7c22f16b7add5f
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-16 12:47:37 +0600

    init

---
+++ jwt-auth-1/src/main/java/com/mahmud/jwtauth1/JwtAuth1Application.java
@@ -0,0 +1,13 @@
+package com.mahmud.jwtauth1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuth1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAuth1Application.class, args);
+    }
+
+}
---
+++ jwt-auth-1/src/test/java/com/mahmud/jwtauth1/JwtAuth1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.jwtauth1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuth1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
