28d8aa5baae4269996bd310ab859908c89aecfb9
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-16 12:56:03 +0600

    Auth Request entity

---
+++ jwt-auth-1/src/main/java/com/mahmud/jwtauth1/entity/AuthRequest.java
@@ -0,0 +1,15 @@
+package com.mahmud.jwtauth1.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+}
