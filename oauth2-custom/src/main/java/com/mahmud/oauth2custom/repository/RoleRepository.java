beb2269de55a1b0722c8e453319d0e1dedfa7bb6
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-16 12:53:53 +0600

    oauth2 init config

--- oauth2-custom/src/main/java/com/mahmud/oauth2custom/repository/RoleRepository.java
+++ oauth2-custom/src/main/java/com/mahmud/oauth2custom/repository/RoleRepository.java
@@ -2,6 +2,8 @@ package com.mahmud.oauth2custom.repository;
 
 import com.mahmud.oauth2custom.models.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role,Integer> {
 }
