7c6875e05763d619b42e4dd54de346e6947b8bac
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-18 22:59:48 +0600

    added working receive method

--- spring-jms-artemis/src/main/java/com/mahmud/springjmsartemis/JmsMessagingService.java
+++ spring-jms-artemis/src/main/java/com/mahmud/springjmsartemis/JmsMessagingService.java
@@ -1,17 +1,39 @@
 package com.mahmud.springjmsartemis;
 
+import jakarta.jms.Message;
+import jakarta.jms.TextMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
-public class JmsMessagingService {
+@Component
+public class JmsMessagingService  {
     private JmsTemplate jmsTemplate;
+    private MessageConverter converter;
 
-    public JmsMessagingService(JmsTemplate jmsTemplate) {
+    @Autowired
+    public JmsMessagingService(JmsTemplate jmsTemplate, MessageConverter converter) {
         this.jmsTemplate = jmsTemplate;
+        this.converter = converter;
     }
 
-    public void sendMessage(String msgPayload) {
-        jmsTemplate.send(session -> session.createTextMessage(msgPayload));
+
+    public void sendMessage(CustomMessage msgPayload) {
+        jmsTemplate.convertAndSend("testQueue", msgPayload);
+    }
+
+    public String receiveMessage() throws Exception {
+        CustomMessage message = (CustomMessage) jmsTemplate.receiveAndConvert("testQueue");
+        System.out.println(message.getPayLoad());
+        return message.getPayLoad();
+//        Message message = jmsTemplate.receive();
+//        System.out.println(message);
+//        return (CustomMessage) converter.fromMessage(message);
     }
 }
