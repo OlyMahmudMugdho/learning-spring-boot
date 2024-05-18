8f0dcd942a5e343323485de7e40d8fc8df904889
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-18 14:35:36 +0600

    init

---
+++ spring-jms-artemis/src/main/java/com/mahmud/springjmsartemis/SpringJmsArtemisApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.springjmsartemis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJmsArtemisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJmsArtemisApplication.class, args);
+	}
+
+}
---
+++ spring-jms-artemis/src/test/java/com/mahmud/springjmsartemis/SpringJmsArtemisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.springjmsartemis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJmsArtemisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
