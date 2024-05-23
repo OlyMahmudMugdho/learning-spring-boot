f2575eee33a2ec04cda6eb64be6ed870b5206664
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-23 21:36:40 +0600

    file writer handler

--- simple-spring-integration/src/main/java/com/mahmud/simplespringintegration/configurations/FileWriterIntegrationConfiguration.java
+++ simple-spring-integration/src/main/java/com/mahmud/simplespringintegration/configurations/FileWriterIntegrationConfiguration.java
@@ -2,8 +2,13 @@ package com.mahmud.simplespringintegration.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.annotation.Transformer;
 import org.springframework.integration.core.GenericTransformer;
+import org.springframework.integration.file.FileWritingMessageHandler;
+import org.springframework.integration.file.support.FileExistsMode;
+
+import java.io.File;
 
 @Configuration
 public class FileWriterIntegrationConfiguration {
@@ -14,5 +19,15 @@ public class FileWriterIntegrationConfiguration {
     )
     public GenericTransformer<String, String> upperCaseTransformer() {
         return text -> text.toUpperCase();
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "fileWriterChannel")
+    public FileWritingMessageHandler fileWriter() {
+        FileWritingMessageHandler handler = new FileWritingMessageHandler(new File("/home/$USER/temp/files"));
+        handler.setExpectReply(false);
+        handler.setFileExistsMode(FileExistsMode.APPEND);
+        handler.setAppendNewLine(true);
+        return handler;
     }
 }
