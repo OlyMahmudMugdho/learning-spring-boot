8d5f09dd455487f95e60690c9a80e6a0d67fe4cd
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-23 21:48:48 +0600

    runner to write a sample text file

--- simple-spring-integration/src/main/java/com/mahmud/simplespringintegration/SimpleSpringIntegrationApplication.java
+++ simple-spring-integration/src/main/java/com/mahmud/simplespringintegration/SimpleSpringIntegrationApplication.java
@@ -1,13 +1,25 @@
 package com.mahmud.simplespringintegration;
 
+import com.mahmud.simplespringintegration.gateway.FileWriterGateway;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleSpringIntegrationApplication {
+public class SimpleSpringIntegrationApplication implements CommandLineRunner {
+
+    private final FileWriterGateway fileWriterGateway;
+
+    public SimpleSpringIntegrationApplication(FileWriterGateway fileWriterGateway) {
+        this.fileWriterGateway = fileWriterGateway;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SimpleSpringIntegrationApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        fileWriterGateway.writeToFile("test.txt", "ok");
+    }
 }
