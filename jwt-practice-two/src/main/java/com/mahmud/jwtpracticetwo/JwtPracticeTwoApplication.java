3e6447bbf20707e5b1127c3621d94eba2e9928b4
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-29 12:40:11 +0600

    initial setup

---
+++ jwt-practice-two/src/main/java/com/mahmud/jwtpracticetwo/JwtPracticeTwoApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.jwtpracticetwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtPracticeTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtPracticeTwoApplication.class, args);
+    }
+
+}
---
+++ jwt-practice-two/src/test/java/com/mahmud/jwtpracticetwo/JwtPracticeTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.jwtpracticetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtPracticeTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
