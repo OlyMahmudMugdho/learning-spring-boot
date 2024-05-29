ef24ef4c2f8231df9d698c64d9837e66b0e3d6d2
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-29 17:37:30 +0600

    crud repo

--- jwt-practice-two/src/main/java/com/mahmud/jwtpracticetwo/repositories/UserRepository.java
+++ jwt-practice-two/src/main/java/com/mahmud/jwtpracticetwo/repositories/UserRepository.java
@@ -2,9 +2,12 @@ package com.mahmud.jwtpracticetwo.repositories;
 
 import com.mahmud.jwtpracticetwo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 }
