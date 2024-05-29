b24f38200b79cc46d72b4a37439556b2827f19ea
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-29 17:38:46 +0600

    set password encoder

--- jwt-practice-two/src/main/java/com/mahmud/jwtpracticetwo/services/UserInfoService.java
+++ jwt-practice-two/src/main/java/com/mahmud/jwtpracticetwo/services/UserInfoService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -15,6 +16,9 @@ public class UserInfoService implements UserDetailsService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
 
     @Override
@@ -27,6 +31,7 @@ public class UserInfoService implements UserDetailsService {
     }
 
     public User addUser(User user){
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
