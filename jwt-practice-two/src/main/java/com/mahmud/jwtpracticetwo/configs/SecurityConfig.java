bfaba4a0b40b00d3f95c0043d1515ee369b39e20
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-29 13:16:24 +0600

    password encoder bean

--- jwt-practice-two/src/main/java/com/mahmud/jwtpracticetwo/configs/SecurityConfig.java
+++ jwt-practice-two/src/main/java/com/mahmud/jwtpracticetwo/configs/SecurityConfig.java
@@ -1,5 +1,14 @@
 package com.mahmud.jwtpracticetwo.configs;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
