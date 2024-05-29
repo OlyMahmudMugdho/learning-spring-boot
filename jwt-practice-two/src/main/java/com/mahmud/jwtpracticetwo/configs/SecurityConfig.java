18120e1c297dd818d8e721b0f8c6b35f50ca7b89
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-29 13:59:29 +0600

    added authentication provider

--- jwt-practice-two/src/main/java/com/mahmud/jwtpracticetwo/configs/SecurityConfig.java
+++ jwt-practice-two/src/main/java/com/mahmud/jwtpracticetwo/configs/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.mahmud.jwtpracticetwo.configs;
 
+import com.mahmud.jwtpracticetwo.services.UserInfoService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,4 +18,19 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserInfoService();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService());
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+
 }
