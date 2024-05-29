1e60c022c6ea759c90dbf0c2c556b7287405a781
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-29 13:21:35 +0600

    entity updated

--- jwt-practice-two/src/main/java/com/mahmud/jwtpracticetwo/models/User.java
+++ jwt-practice-two/src/main/java/com/mahmud/jwtpracticetwo/models/User.java
@@ -17,6 +17,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+    private String username;
     private String email;
     private String password;
     private String roles;
