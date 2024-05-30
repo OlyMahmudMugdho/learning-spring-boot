f635dc732ba4f30761743e9f8a109f0eb35ea64b
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-30 21:54:57 +0600

    initial setup

---
+++ reactive-api/src/main/java/com/mahmud/reactiveapi/ReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.reactiveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveApiApplication.class, args);
+    }
+
+}
