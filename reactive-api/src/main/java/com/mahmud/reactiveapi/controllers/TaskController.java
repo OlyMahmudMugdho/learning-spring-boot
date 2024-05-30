2466b309ff0c7fca1532d0c71dd486dc6951f9cc
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-30 22:25:16 +0600

    create task endpoint

---
+++ reactive-api/src/main/java/com/mahmud/reactiveapi/controllers/TaskController.java
@@ -0,0 +1,28 @@
+package com.mahmud.reactiveapi.controllers;
+
+import com.mahmud.reactiveapi.entity.Todo;
+import com.mahmud.reactiveapi.model.Tasks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+    private Tasks tasks;
+
+    @Autowired
+    public TaskController(Tasks tasks) {
+        this.tasks = tasks;
+    }
+
+
+    @PostMapping
+    public Mono<Todo> createTask(@RequestBody Todo task) {
+        tasks.addTodo(task);
+        return Mono.just(task);
+    }
+}
