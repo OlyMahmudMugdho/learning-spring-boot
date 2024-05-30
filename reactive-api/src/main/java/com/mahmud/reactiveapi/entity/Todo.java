f165f52ca2f964ed1c00cc9a89c347d34ec12a85
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-30 22:06:40 +0600

    Todo entity

---
+++ reactive-api/src/main/java/com/mahmud/reactiveapi/entity/Todo.java
@@ -0,0 +1,16 @@
+package com.mahmud.reactiveapi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Todo {
+    private String id;
+    private String title;
+    private String description;
+}
