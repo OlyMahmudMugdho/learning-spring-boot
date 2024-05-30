87359bc176ef6872612a7e089f0663248792a280
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-30 22:23:57 +0600

    Todo entity updated

--- reactive-api/src/main/java/com/mahmud/reactiveapi/entity/Todo.java
+++ reactive-api/src/main/java/com/mahmud/reactiveapi/entity/Todo.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class Todo {
-    private String id;
+    private int id;
     private String title;
     private String description;
 }
