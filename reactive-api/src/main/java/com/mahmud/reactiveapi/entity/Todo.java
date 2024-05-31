99d210a7dee091ede05ba0efc9e02b73a199e015
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-05-31 17:11:56 +0600

    remove builder

--- reactive-api/src/main/java/com/mahmud/reactiveapi/entity/Todo.java
+++ reactive-api/src/main/java/com/mahmud/reactiveapi/entity/Todo.java
@@ -1,14 +1,12 @@
 package com.mahmud.reactiveapi.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class Todo {
     private int id;
     private String title;
