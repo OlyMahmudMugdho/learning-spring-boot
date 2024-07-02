ac9e2bd8f5b294021715730453ddf228b62a42b6
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-03 00:20:13 +0600

    init

---
+++ kafka-sping-boot/src/main/java/com/mahmud/kafka_sping_boot/KafkaSpingBootApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.kafka_sping_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaSpingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaSpingBootApplication.class, args);
+	}
+
+}
---
+++ kafka-sping-boot/src/test/java/com/mahmud/kafka_sping_boot/KafkaSpingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.kafka_sping_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpingBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
