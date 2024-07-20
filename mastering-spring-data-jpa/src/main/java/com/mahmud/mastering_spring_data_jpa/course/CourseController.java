529dfb59b477145f4f12f1b4dff543b95acf40bb
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-20 22:24:50 +0600

    get all course method

--- mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/course/CourseController.java
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/course/CourseController.java
@@ -2,6 +2,8 @@ package com.mahmud.mastering_spring_data_jpa.course;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/course")
 public class CourseController {
@@ -9,6 +11,11 @@ public class CourseController {
 
     public CourseController(CourseRepository courseRepository) {
         this.courseRepository = courseRepository;
+    }
+
+    @GetMapping
+    public List<Course> getCourses() {
+        return courseRepository.findAll();
     }
 
     @PostMapping
