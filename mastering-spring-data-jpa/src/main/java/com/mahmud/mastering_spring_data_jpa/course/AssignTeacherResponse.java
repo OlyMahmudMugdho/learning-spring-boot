9058531fbbf74c1fb105c14473fe7c95610a47f3
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-20 22:52:13 +0600

    Assign Teacher Response

--- mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/course/AssignTeacherResponse.java
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/course/AssignTeacherResponse.java
@@ -1,7 +1,13 @@
 package com.mahmud.mastering_spring_data_jpa.course;
 
 import com.mahmud.mastering_spring_data_jpa.teacher.Teacher;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AssignTeacherResponse {
     private Teacher teacher;
     private Course course;
