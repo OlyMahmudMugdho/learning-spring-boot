132278ed948d6a8c11d4ace9b622ab8476c9221c
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-20 22:52:32 +0600

    Assign Teacher Controller

--- mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/course/CourseController.java
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/course/CourseController.java
@@ -1,5 +1,6 @@
 package com.mahmud.mastering_spring_data_jpa.course;
 
+import com.mahmud.mastering_spring_data_jpa.teacher.Teacher;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,6 +21,14 @@ public class CourseController {
 
     @PostMapping
     public Course createCourse(@RequestBody Course course) {
+        return courseRepository.save(course);
+    }
+
+    @PostMapping("/assign")
+    public Course assignTeacher(@RequestBody AssignTeacherResponse assignTeacherResponse){
+        Course course = assignTeacherResponse.getCourse();
+        Teacher teacher = assignTeacherResponse.getTeacher();
+        course.setTeacher(teacher);
         return courseRepository.save(course);
     }
 }
