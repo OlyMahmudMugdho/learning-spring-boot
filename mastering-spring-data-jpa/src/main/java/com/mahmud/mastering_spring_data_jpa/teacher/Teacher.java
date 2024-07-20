35adeaaf3beee3b9beea3da5478d60706e1d3206
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-20 22:17:14 +0600

    JsonIgnore on id

--- mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/teacher/Teacher.java
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/teacher/Teacher.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class Teacher {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonIgnore
     private Integer id;
     private String name;
     private String username;
