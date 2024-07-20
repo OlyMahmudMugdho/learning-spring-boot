8985c071f029ee9d66371cb62cb021b0b8fab2a5
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-20 22:10:06 +0600

    create teacher endpoint

--- mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/teacher/TeacherController.java
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/teacher/TeacherController.java
@@ -1,8 +1,21 @@
 package com.mahmud.mastering_spring_data_jpa.teacher;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-
+@RequestMapping("/api/v1/teacher")
 public class TeacherController {
+    private final TeacherRepository teacherRepository;
+
+    public TeacherController(TeacherRepository teacherRepository) {
+        this.teacherRepository = teacherRepository;
+    }
+
+    @PostMapping("")
+    public Teacher createTeacher(@RequestBody Teacher teacher) {
+        return teacherRepository.save(teacher);
+    }
 }
