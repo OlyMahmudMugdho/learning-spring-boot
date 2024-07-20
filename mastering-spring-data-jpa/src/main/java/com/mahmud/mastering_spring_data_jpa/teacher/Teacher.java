efc7707508ea5e9c70f1178a91fbdc313a3900b3
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-20 22:04:37 +0600

    Teacher entity

---
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/teacher/Teacher.java
@@ -0,0 +1,23 @@
+package com.mahmud.mastering_spring_data_jpa.teacher;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.mahmud.mastering_spring_data_jpa.course.Course;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Entity
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String username;
+    @OneToMany(mappedBy = "teacher")
+    @JsonIgnore
+    private List<Course> courses;
+}
