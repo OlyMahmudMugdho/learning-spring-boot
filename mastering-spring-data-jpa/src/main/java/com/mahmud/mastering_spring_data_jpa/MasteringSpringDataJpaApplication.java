ffe705f9d0db2708a263c0603251208d639fb386
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-20 21:49:40 +0600

    db conn

---
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/MasteringSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.mastering_spring_data_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasteringSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasteringSpringDataJpaApplication.class, args);
+	}
+
+}
---
+++ mastering-spring-data-jpa/src/test/java/com/mahmud/mastering_spring_data_jpa/MasteringSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.mastering_spring_data_jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasteringSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
