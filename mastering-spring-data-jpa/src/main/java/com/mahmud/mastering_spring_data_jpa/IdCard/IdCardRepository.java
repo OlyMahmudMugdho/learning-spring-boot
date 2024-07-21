fa58860dc003e2c22f01b24630a22786ece63ae7
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-21 13:56:29 +0600

    id card repo

---
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/IdCard/IdCardRepository.java
@@ -0,0 +1,6 @@
+package com.mahmud.mastering_spring_data_jpa.IdCard;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IdCardRepository extends JpaRepository<IdCard, Long> {
+}
