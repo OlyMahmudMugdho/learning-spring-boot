4c8c48bb0e1b76f4602d63a8eda7b56f1861d276
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-21 13:57:45 +0600

    create id card method

--- mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/IdCard/IdCardController.java
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/IdCard/IdCardController.java
@@ -1,5 +1,7 @@
 package com.mahmud.mastering_spring_data_jpa.IdCard;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,10 @@ public class IdCardController {
 
     public IdCardController(IdCardRepository idCardRepository) {
         this.idCardRepository = idCardRepository;
+    }
+
+    @PostMapping
+    public IdCard createIdCard(@RequestBody IdCard idCard) {
+        return idCardRepository.save(idCard);
     }
 }
