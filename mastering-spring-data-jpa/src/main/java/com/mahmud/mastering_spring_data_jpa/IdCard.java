5ebf2f16d1c03209c0d1cd163b79033e04cc7ef7
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-21 13:46:40 +0600

    ID Card Entity

---
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/IdCard.java
@@ -0,0 +1,22 @@
+package com.mahmud.mastering_spring_data_jpa;
+
+import com.mahmud.mastering_spring_data_jpa.student.Student;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class IdCard {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "card_number")
+    private Long cardNo;
+    @OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "student_id")
+    private Student student;
+}
+
