b829c3dd253f265e3ba7ef19bba534082c2335c5
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-22 17:04:46 +0600

    get single video method

--- mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/resource/VideoController.java
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/resource/VideoController.java
@@ -3,6 +3,7 @@ package com.mahmud.mastering_spring_data_jpa.resource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/video")
@@ -16,6 +17,12 @@ public class VideoController {
     @GetMapping
     public List<Video> getAllVideos() {
         return videoRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Video getVideo(@PathVariable Integer id) {
+        Optional<Video> video = videoRepository.findById(id);
+        return video.orElse(null);
     }
 
     @PostMapping
