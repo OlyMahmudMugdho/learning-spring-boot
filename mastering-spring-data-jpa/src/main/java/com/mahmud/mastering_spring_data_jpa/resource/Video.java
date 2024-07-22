7aeeb2dba0fe4f167d13a37f610a7e64b31ab38c
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-22 16:00:22 +0600

    Video inherits from Base Entity (Resource)

---
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/resource/Video.java
@@ -0,0 +1,17 @@
+package com.mahmud.mastering_spring_data_jpa.resource;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@Entity
+public class Video extends Resource{
+}
