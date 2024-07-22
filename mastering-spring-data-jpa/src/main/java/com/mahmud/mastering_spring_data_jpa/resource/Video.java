91010181c979d84d9e8d70fc4758fc56c6ad78f2
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-22 16:35:18 +0600

    add duration field

--- mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/resource/Video.java
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/resource/Video.java
@@ -8,10 +8,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
 @Entity
 public class Video extends Resource{
+    private Long durationInSeconds;
 }
