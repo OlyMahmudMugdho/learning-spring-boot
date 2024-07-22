b458438ba5a00ee01aa9975e0a95ede3d1399205
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-22 16:34:08 +0600

    rm ResourceRepository

--- mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/resource/ResourceRepository.java
+++
@@ -1,6 +0,0 @@
-package com.mahmud.mastering_spring_data_jpa.resource;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResourceRepository extends JpaRepository<Resource, Integer> {
-}
