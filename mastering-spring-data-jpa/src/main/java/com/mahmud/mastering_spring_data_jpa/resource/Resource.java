e99264f5fd27a50227517ce97c25df0f633f1566
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-22 16:34:44 +0600

    Resource Entity (base entity)

--- mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/resource/Resource.java
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/resource/Resource.java
@@ -1,15 +1,28 @@
 package com.mahmud.mastering_spring_data_jpa.resource;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @MappedSuperclass
+@SuperBuilder
 public class Resource {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String type;
     private String description;
+
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 }
