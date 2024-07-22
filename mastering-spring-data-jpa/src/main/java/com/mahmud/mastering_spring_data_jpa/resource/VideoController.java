9747f0a884f5dbc8d950ab629f79b5fbdae3437d
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-07-22 17:17:49 +0600

    update Video method

--- mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/resource/VideoController.java
+++ mastering-spring-data-jpa/src/main/java/com/mahmud/mastering_spring_data_jpa/resource/VideoController.java
@@ -2,6 +2,7 @@ package com.mahmud.mastering_spring_data_jpa.resource;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +28,18 @@ public class VideoController {
 
     @PostMapping
     public Video createVideo(@RequestBody Video video){
+        video.setCreatedAt(LocalDateTime.now());
+        video.setCreatedAt(LocalDateTime.now());
+        return videoRepository.save(video);
+    }
+
+    @PutMapping("{id}")
+    public Video updateVideo(@PathVariable Integer id, @RequestBody Video videoBody) {
+        Video video = this.getVideo(id);
+        if (video == null) return null;
+        video.setType(videoBody.getType());
+        video.setDescription(videoBody.getDescription());
+        video.setUpdatedAt(LocalDateTime.now());
         return videoRepository.save(video);
     }
 
