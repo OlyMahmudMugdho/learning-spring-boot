cbec7fade52562e7de4a05b384718da3e9d94ad1
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-08-13 20:24:22 +0600

    email sender service

---
+++ sending-email/src/main/java/com/mahmud/sending_email/services/MailSenderService.java
@@ -0,0 +1,22 @@
+package com.mahmud.sending_email.services;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSenderService {
+    private final JavaMailSender mailSender;
+
+    public MailSenderService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void sendNewMail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
