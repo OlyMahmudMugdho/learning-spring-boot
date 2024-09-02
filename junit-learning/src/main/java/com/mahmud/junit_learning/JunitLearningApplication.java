daf6ae5675992064ed8f9e181de67dc4f95774b9
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-02 20:56:55 +0600

    project init

---
+++ junit-learning/src/main/java/com/mahmud/junit_learning/JunitLearningApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.junit_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitLearningApplication.class, args);
+	}
+
+}
---
+++ junit-learning/src/test/java/com/mahmud/junit_learning/JunitLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.junit_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
