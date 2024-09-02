8644401f049c54ce655eda943164ee2a08c660e3
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-02 21:26:11 +0600

    complete author service

--- junit-learning/src/main/java/com/mahmud/junit_learning/service/AuthorService.java
+++ junit-learning/src/main/java/com/mahmud/junit_learning/service/AuthorService.java
@@ -1,7 +1,36 @@
 package com.mahmud.junit_learning.service;
 
+import com.mahmud.junit_learning.model.Author;
+import com.mahmud.junit_learning.repository.AuthorRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuthorService {
+    private final AuthorRepository authorRepository;
+
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public Author addAuthor(Author author) {
+        return authorRepository.save(author);
+    }
+
+    public List<Author> getAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public Author getAuthorById(Long id) {
+        return authorRepository.findById(id);
+    }
+
+    public Author updateAuthor(Author author) {
+        return authorRepository.update(author);
+    }
+
+    public void deleteAuthor(Long id) {
+        authorRepository.delete(id);
+    }
 }
