73a1f412f6b8a863c5376fe73b87c67e619bf3db
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-03 12:22:52 +0600

    bug fix

--- junit-learning/src/main/java/com/mahmud/junit_learning/controller/BookController.java
+++ junit-learning/src/main/java/com/mahmud/junit_learning/controller/BookController.java
@@ -1,10 +1,36 @@
 package com.mahmud.junit_learning.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.mahmud.junit_learning.model.Author;
+import com.mahmud.junit_learning.model.Book;
+import com.mahmud.junit_learning.service.AuthorService;
+import com.mahmud.junit_learning.service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/book")
 public class BookController {
+    private final BookService bookService;
+    private final AuthorService authorService;
+
+    public BookController(BookService bookService, AuthorService authorService) {
+        this.bookService = bookService;
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return bookService.getAllBooks();
+    }
+
+    @PostMapping
+    public Book createBook(@RequestBody Book book) {
+        Author author = authorService.getAuthorById(book.getAuthor().getId());
+        System.out.println(author);
+        book.setAuthor(author);
+
+        return bookService.addBook(book);
+    }
+
 }
