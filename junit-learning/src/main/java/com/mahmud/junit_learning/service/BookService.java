ec1f3af02a66dc4bcbdc61d8fd8a8443e8e01d01
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-03 12:22:06 +0600

    book service

--- junit-learning/src/main/java/com/mahmud/junit_learning/service/BookService.java
+++ junit-learning/src/main/java/com/mahmud/junit_learning/service/BookService.java
@@ -1,7 +1,41 @@
 package com.mahmud.junit_learning.service;
 
+import com.mahmud.junit_learning.model.Book;
+import com.mahmud.junit_learning.repository.BookRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class BookService {
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book getBookById(Long id) {
+        return bookRepository.findById(id);
+    }
+
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public Book updateBook(Book book) {
+        return bookRepository.save(book);
+    }
+
+    public void deleteBookById(Long id) {
+        bookRepository.delete(id);
+    }
+
+    public List<Book> findBooksByAuthor(Long authorId) {
+        return bookRepository.findBooksByAuthor(authorId);
+    }
 }
