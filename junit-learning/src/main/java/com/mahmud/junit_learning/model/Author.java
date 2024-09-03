c235c3b15473b54071bce69b00d77b57a3442eb5
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-03 12:22:30 +0600

    author entity update

--- junit-learning/src/main/java/com/mahmud/junit_learning/model/Author.java
+++ junit-learning/src/main/java/com/mahmud/junit_learning/model/Author.java
@@ -1,12 +1,11 @@
 package com.mahmud.junit_learning.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Author {
