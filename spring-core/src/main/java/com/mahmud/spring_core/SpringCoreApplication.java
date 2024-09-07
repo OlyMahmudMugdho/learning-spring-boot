d590ce49473c7063e8cc2273b79d9b5d64ccf278
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-09-07 16:36:45 +0600

    spring core init

---
+++ spring-core/src/main/java/com/mahmud/spring_core/SpringCoreApplication.java
@@ -0,0 +1,18 @@
+package com.mahmud.spring_core;
+
+import com.mahmud.spring_core.config.PrinterConfig;
+import com.mahmud.spring_core.printer.Printer;
+import com.mahmud.spring_core.printer.impl.BluePrinter;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class SpringCoreApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context =  new AnnotationConfigApplicationContext(PrinterConfig.class);
+		Printer printer = context.getBean(Printer.class);
+		printer.print("name");
+	}
+
+}
