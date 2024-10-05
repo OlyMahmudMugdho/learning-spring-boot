c40ee07af9f949218c581562630420ffd1861de7
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-10-05 21:58:51 +0600

    config

---
+++ kafka-pub-sub/src/test/java/com/mahmud/kafka_pub_sub/KafkaPubSubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.kafka_pub_sub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaPubSubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ stripe-payment/src/test/java/com/mahmud/stripe_payment/StripePaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.stripe_payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripePaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
