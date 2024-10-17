6657b359fe963213e7c3a930fa0d483534989f09
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-10-17 21:02:06 +0600

    empty project

---
+++ jpa-relationship/src/main/java/com/mahmud/jpa_relationship/JpaRelationshipApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.jpa_relationship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRelationshipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaRelationshipApplication.class, args);
+	}
+
+}
---
+++ jpa-relationship/src/test/java/com/mahmud/jpa_relationship/JpaRelationshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.jpa_relationship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRelationshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
