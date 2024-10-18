563fe5747f0feeefecd2a3c0f530fd98dd45acca
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-10-18 12:28:42 +0600

    add Account controller

---
+++ jpa-relationship/src/main/java/com/mahmud/jpa_relationship/controllers/AccountController.java
@@ -0,0 +1,23 @@
+package com.mahmud.jpa_relationship.controllers;
+
+import com.mahmud.jpa_relationship.models.Account;
+import com.mahmud.jpa_relationship.services.AccountService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = {"/api/account/","/api/account"})
+public class AccountController {
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping
+    public Account createAccount(@RequestBody Account account) {
+        return accountService.createAccount(account);
+    }
+}
