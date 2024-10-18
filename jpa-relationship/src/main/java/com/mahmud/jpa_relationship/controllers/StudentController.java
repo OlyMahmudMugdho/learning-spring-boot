319d93338ed9248362282bebf8472abe4262144c
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-10-18 12:27:57 +0600

    StudentDto to Student conversion in student creation

--- jpa-relationship/src/main/java/com/mahmud/jpa_relationship/controllers/StudentController.java
+++ jpa-relationship/src/main/java/com/mahmud/jpa_relationship/controllers/StudentController.java
@@ -1,7 +1,10 @@
 package com.mahmud.jpa_relationship.controllers;
 
 import com.mahmud.jpa_relationship.models.Student;
+import com.mahmud.jpa_relationship.models.dto.StudentDto;
+import com.mahmud.jpa_relationship.models.mappers.StudentMapper;
 import com.mahmud.jpa_relationship.services.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class StudentController {
 
     private final StudentService studentService;
+    private final StudentMapper studentMapper;
 
-    public StudentController(StudentService studentService) {
+    public StudentController(StudentService studentService, StudentMapper studentMapper) {
         this.studentService = studentService;
+        this.studentMapper = studentMapper;
     }
 
     @PostMapping
-    public Student createStudent(@RequestBody Student student) {
+    public Student createStudent(@RequestBody StudentDto studentDto) {
+        Student student = studentMapper.maptoStudent(studentDto);
         return studentService.createStudent(student);
     }
 }
