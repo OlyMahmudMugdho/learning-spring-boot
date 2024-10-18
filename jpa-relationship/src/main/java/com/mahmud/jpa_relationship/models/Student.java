f05809c2fbab024a53a9f3f02d7b86246a2b8de6
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-10-18 12:24:50 +0600

    Student entity updated

--- jpa-relationship/src/main/java/com/mahmud/jpa_relationship/models/Student.java
+++ jpa-relationship/src/main/java/com/mahmud/jpa_relationship/models/Student.java
@@ -1,9 +1,6 @@
 package com.mahmud.jpa_relationship.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +11,10 @@ import lombok.NoArgsConstructor;
 @Data
 public class Student {
     @Id
+    @GeneratedValue(
+            strategy = GenerationType.AUTO
+    )
+    @Column(name = "student_id")
     private Integer studentId;
     private String firstName;
     private String lastName;
