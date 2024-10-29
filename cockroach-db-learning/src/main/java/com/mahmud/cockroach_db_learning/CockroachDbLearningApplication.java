6bb6b7a927ad1cd1b1f5deb7d13d87ed054db57d
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-10-29 19:42:14 +0600

    cockroach db project init

---
+++ cockroach-db-learning/src/main/java/com/mahmud/cockroach_db_learning/CockroachDbLearningApplication.java
@@ -0,0 +1,13 @@
+package com.mahmud.cockroach_db_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CockroachDbLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CockroachDbLearningApplication.class, args);
+	}
+
+}
---
+++ cockroach-db-learning/src/test/java/com/mahmud/cockroach_db_learning/CockroachDbLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.cockroach_db_learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CockroachDbLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
