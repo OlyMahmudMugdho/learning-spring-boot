1d7f2e99eb03e71a61af0f7a7a26d9859a65706b
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-11-01 12:36:07 +0600

    spring-data-mongo-db initial

---
+++ spring-data-mongo-db/src/main/java/com/mahmud/spring_data_mongo_db/SpringDataMongoDbApplication.java
@@ -0,0 +1,35 @@
+package com.mahmud.spring_data_mongo_db;
+
+import com.mahmud.spring_data_mongo_db.models.Student;
+import com.mahmud.spring_data_mongo_db.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories(basePackages = "com.mahmud.spring_data_mongo_db")
+public class SpringDataMongoDbApplication implements CommandLineRunner {
+
+	public MongoTemplate mongoTemplate;
+
+	@Autowired
+	public SpringDataMongoDbApplication(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataMongoDbApplication.class, args);
+	}
+
+	@Override
+	public void run(String[] args) throws Exception {
+		Student student = new Student();
+		student.setName("M. Oly Mahmud");
+		mongoTemplate.save(student, "student");
+	}
+}
+
---
+++ spring-data-mongo-db/src/test/java/com/mahmud/spring_data_mongo_db/SpringDataMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmud.spring_data_mongo_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
