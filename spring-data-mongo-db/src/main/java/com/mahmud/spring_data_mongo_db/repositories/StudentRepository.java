096056e49630518d835b231b1dad69f775fba00e
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-11-02 12:23:03 +0600

    added findFirstByRoll method

--- spring-data-mongo-db/src/main/java/com/mahmud/spring_data_mongo_db/repositories/StudentRepository.java
+++ spring-data-mongo-db/src/main/java/com/mahmud/spring_data_mongo_db/repositories/StudentRepository.java
@@ -4,4 +4,5 @@ import com.mahmud.spring_data_mongo_db.models.Student;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface StudentRepository extends MongoRepository<Student, String> {
+    Student findFirstByRoll(Integer roll);
 }
